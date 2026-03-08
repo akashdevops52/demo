05ae80ad66d6f195b396c6856c806a2c313a04d7
Akash <akashdevops52@gmail.com>
2026-03-08 13:13:45 +0530

    initail code

---
+++ src/main/java/com/devops/demo/DevpsDevlomentApplication.java
@@ -0,0 +1,20 @@
+package com.devops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevpsDevlomentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevpsDevlomentApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "<h1>Hello there! Welcome to my DevOps application</h1>";
+    }
+}
